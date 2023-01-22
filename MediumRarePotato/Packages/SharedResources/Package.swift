1f4e7862ad7752a75174d12bd7acc763497b9d5f
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-22 18:06:01 +1300

    Base project setup complete

--- MediumRarePotato/Packages/SharedResources/Package.swift
+++ MediumRarePotato/Packages/SharedResources/Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SharedResources",
-            targets: ["SharedResources"]),
+            targets: ["SharedResources"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
