aaf2475f8b227ca3f23e6edfe2da628e3fc6bb17
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-26 15:05:46 +1300

    Basic version of the custom text input window

--- MediumRarePotato/Packages/UIComponents/Sources/UIComponents/CustomTextField.swift
+++ MediumRarePotato/Packages/UIComponents/Sources/UIComponents/CustomTextField.swift
@@ -1,16 +1,38 @@
 import UIKit
 import SnapKit
-import SharedExtensions
+import SharedResources
 
 public class CustomTextField: UIView {
 
+    var placeholder: String?
+    weak var delegate: UITextFieldDelegate?
+
     private lazy var backgroundView: UIView = {
         let view = UIView()
-
+        view.isUserInteractionEnabled = true
         return view
     }()
 
-    override init(frame: CGRect) {
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.font = UIFont(name: FontFamily.AmaticSC.regular.name, size: 25)
+        textField.textColor = Asset.Colors.secondaryTextColor.color
+        textField.textAlignment = .center
+        textField.contentVerticalAlignment = .bottom
+        textField.borderStyle = .none
+        textField.delegate = delegate
+        if let placeholder = placeholder {
+            textField.attributedPlaceholder = NSMutableAttributedString(
+                string: placeholder,
+                attributes: [NSAttributedString.Key.foregroundColor: Asset.Colors.placeholderColor.color]
+            )
+        }
+        return textField
+    }()
+
+    public init(frame: CGRect, placeholder: String?, delegate: UITextFieldDelegate?) {
+        self.placeholder = placeholder
+        self.delegate = delegate
         super.init(frame: frame)
         setupView()
     }
@@ -19,10 +41,27 @@ public class CustomTextField: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override public func layoutSubviews() {
+        addBottomLine(to: textField)
+    }
+
     func setupView() {
         self.addSubview(backgroundView)
+        backgroundView.addSubview(textField)
         backgroundView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+
+        textField.snp.makeConstraints { make in
+            make.top.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(10)
+        }
+    }
+
+    private func addBottomLine(to textField: UITextField) {
+        let layer = CALayer()
+        layer.frame = CGRect(x: 0, y: textField.frame.height - 1, width: textField.frame.width, height: 1)
+        layer.backgroundColor = Asset.Colors.primaryTextColor.color.cgColor
+        textField.layer.addSublayer(layer)
     }
 }
