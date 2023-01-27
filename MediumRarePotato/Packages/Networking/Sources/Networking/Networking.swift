9706d2a580eb30fe38a0264b537b0ab222b1799d
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-27 19:37:32 +1300

    Basic networking request

--- MediumRarePotato/Packages/Networking/Sources/Networking/Networking.swift
+++ MediumRarePotato/Packages/Networking/Sources/Networking/Networking.swift
@@ -1,6 +1,25 @@
-public struct Networking {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import Alamofire
 
-    public init() {
+final class NetworkManager {
+
+}
+
+// MARK: Private Methods
+extension NetworkManager {
+    private func execute(parameters: [String: String]) {
+        AF.request("https://game.medium-rare-potato.io/", method: .get, parameters: parameters).response { response in
+            print(response)
+        }
+    }
+}
+
+// MARK: Public Methods
+extension NetworkManager {
+    public func login(apiKey: String) {
+        let parameters: [String: String] = [
+            "api-key": apiKey
+        ]
+        execute(parameters: parameters)
     }
 }
