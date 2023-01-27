721f6919d627e0e9e3ee1463b5efd8f0b592ebb2
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-28 02:12:12 +1300

    Added SFSymbols and Colors for toast message

--- MediumRarePotato/Packages/SharedResources/Sources/SharedResources/SFSymbol.swift
+++ MediumRarePotato/Packages/SharedResources/Sources/SharedResources/SFSymbol.swift
@@ -3,4 +3,6 @@ import Foundation
 // MARK: - SFSymbol
 public enum SFSymbol: String, SFSymbolRepresentable {
     case threeLinesHorizontal = "line.3.horizontal"
+    case exclamationMark = "exclamationmark.circle.fill"
+    case checkMarkCircle = "checkmark.circle.fill"
 }
