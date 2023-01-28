aca1bd3c0f2370ca995f1507ea91b52eb44e6ff1
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-01-29 02:03:12 +1300

    Network Request

--- MediumRarePotato/Packages/Networking/Sources/Networking/Networking.swift
+++ MediumRarePotato/Packages/Networking/Sources/Networking/Networking.swift
@@ -2,7 +2,7 @@ import Foundation
 import Alamofire
 
 public protocol NetworkManagerProtocol {
-    func login(apiKey: String, completion: @escaping () -> Void)
+    func login(apiKey: String, completion: @escaping (Data?, Error?) -> Void)
 }
 
 public final class NetworkManager {
@@ -13,21 +13,27 @@ public final class NetworkManager {
 
 // MARK: Private Methods
 extension NetworkManager {
-    private func execute(parameters: HTTPHeaders, completion: @escaping () -> Void) {
+    private func execute(parameters: HTTPHeaders, completion: @escaping (Data?, Error?) -> Void) {
         AF.request("https://game.medium-rare-potato.io/v1/user/",
                    method: .get,
                    headers: parameters
         )
         .response { response in
-            print(response.data)
-            completion()
+            switch response.result {
+
+            case .success(let data):
+                completion(data, nil)
+
+            case .failure(let error):
+                completion(nil, error)
+            }
         }
     }
 }
 
 // MARK: Network Manager Protocol
 extension NetworkManager: NetworkManagerProtocol {
-    public func login(apiKey: String, completion: @escaping () -> Void) {
+    public func login(apiKey: String, completion: @escaping (Data?, Error?) -> Void) {
         let parameters: HTTPHeaders = [
             "api-key": apiKey
         ]
