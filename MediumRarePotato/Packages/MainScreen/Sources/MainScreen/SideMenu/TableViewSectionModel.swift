68dc893b239a330f8ddab55afd2dcfa39794212f
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-02-04 15:02:14 +1300

    Added enum for sections

---
+++ MediumRarePotato/Packages/MainScreen/Sources/MainScreen/SideMenu/TableViewSectionModel.swift
@@ -0,0 +1,23 @@
+import Foundation
+import SharedResources
+
+enum SectionType: CaseIterable {
+    case navigation
+    case languageSelection
+    case currentBalanceDisplay
+}
+
+enum NavigationSection: CaseIterable {
+    case bars
+    case myBars
+    case restaurants
+    case myRestaurants
+    case characters
+    case pvpCooks
+    case dishPool
+    case shop
+    case craft
+    case cakes
+    case referals
+}
+
