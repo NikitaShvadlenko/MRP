1808d974309bfb121f9b6bc7056ae53d35865786
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2023-02-10 05:42:14 +1300

    removed unused label

--- MediumRarePotato/Packages/MyRestaurants/Sources/MyRestaurants/MyRestaurantsView.swift
+++ MediumRarePotato/Packages/MyRestaurants/Sources/MyRestaurants/MyRestaurantsView.swift
@@ -4,19 +4,6 @@ import SnapKit
 
 final class MyRestaurantsView: UIView {
 
-    private lazy var titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = L10n.gameTitle
-        label.font = UIFont(font: FontFamily.AmaticSC.bold, size: 30)
-        label.numberOfLines = 0
-        label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.5
-        label.textAlignment = .left
-        label.sizeToFit()
-        label.textColor = Asset.Colors.primaryTextColor.color
-        return label
-    }()
-
     private lazy var backgroundView: UIImageView = {
         let view = UIImageView()
         view.image = UIImage(asset: Asset.Assets.background)
@@ -38,13 +25,8 @@ final class MyRestaurantsView: UIView {
 extension MyRestaurantsView {
     private func configureViews() {
         addSubview(backgroundView)
-        backgroundView.addSubview(titleLabel)
         backgroundView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
-        }
-        titleLabel.snp.makeConstraints { make in
-            make.top.trailing.leading.equalToSuperview()
-            make.height.equalTo(200)
         }
     }
 }
